19f987d1e6047c03d25c0d0f55c2490b11c13892
benjaminbtrance <benjaminb.tran@gmail.com>
2021-09-10 16:05:48 -0500

    Add workout model and seed the database

---
+++ models/Workout.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+	day: {
+		type: Date,
+		default: Date.now,
+	},
+
+	exercises: [
+		{
+			type: {
+				type: String,
+				trim: true,
+				required: 'Type of exercise is required.',
+			},
+			name: {
+				type: String,
+				trim: true,
+				required: 'Name of exercise is required.',
+			},
+			duration: {
+				type: Number,
+				required: 'Exercise duration is required.',
+			},
+			weight: {
+				type: Number,
+			},
+			reps: {
+				type: Number,
+			},
+			sets: {
+				type: Number,
+			},
+			distance: {
+				type: Number,
+			},
+		},
+	],
+});
+
+const Workout = mongoose.model('Workout', WorkoutSchema);
+module.exports = Workout;
--- server.js
+++ server.js
@@ -2,23 +2,27 @@ const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const path = require('path');
-const PORT = process.env.PORT || 3000;
+
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-mongoose.connect(
-	process.env.MONGDB_URI || 'mongodb://localhost/workout-tracker-db',
-	{
-		useNewUrlParser: true,
-		useFindAndModify: false,
-		useUnifiedTopology: true,
-		useCreateIndex: true,
-	}
-);
+mongoose.connect(process.env.MONGDB_URI || 'mongodb://localhost/workout', {
+	useNewUrlParser: true,
+	useFindAndModify: false,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+});
+
+// // HTML Routes
+// app.use(require('./routes/homeRoutes'));
+
+// // API Routes
+// app.use(require('./routes/apiRoutes.js'));
 
 app.listen(PORT, () => {
 	console.log(`App running on port ${PORT}!`);
