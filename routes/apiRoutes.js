5fb02d24c34b79f5081234b13a6f672387d6af84
benjaminbtrance <benjaminb.tran@gmail.com>
2021-09-12 17:14:08 -0500

    add api/workouts/range to apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,20 @@
-// const router = require('express').Router();
+const Workout = require('../models/Workout.js');
 const router = require('express').Router();
 
-router.post('/api/transaction', ({ body }, res) => {});
+// Route for Dashboard - only display 7 days. Sum up the duration of exercises
+router.get('/api/workouts/range', (req, res) => {
+	Workout.aggregate([
+		{ $addFields: { totalDuration: { $sum: '$exercises.duration' } } },
+	])
+		.sort({ day: -1 })
+		.limit(7)
+		.then((dbWorkout) => {
+			res.json(dbWorkout);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.sendStatus(500);
+		});
+});
 
 module.exports = router;
