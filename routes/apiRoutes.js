079c5aeeb24202030f5a2e88f8b5fc6d888f5755
benjaminbtrance <benjaminb.tran@gmail.com>
2021-09-14 11:21:54 -0500

    add new exercises to a new owrkout plan

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,18 @@
 const router = require('express').Router();
 const Workout = require('../models/Workout.js');
 
+// Add new exercises to a new workout plan
+router.post('/api/workouts', (req, res) => {
+	Workout.create({})
+		.then((dbWorkout) => {
+			res.json(dbWorkout);
+		})
+		.catch((err) => {
+			res.json(err);
+			res.sendStatus(500);
+		});
+});
+
 // View the total duration of each workout from the past seven workouts on the stats page
 router.get('/api/workouts/range', (req, res) => {
 	Workout.aggregate([
