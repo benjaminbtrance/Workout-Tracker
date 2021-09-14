ae7655277ec8bd99c238f969f23844d16fa04e2d
benjaminbtrance <benjaminb.tran@gmail.com>
2021-09-14 12:11:00 -0500

    Add exercises to the most recent workout plan

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -13,6 +13,22 @@ router.post('/api/workouts', (req, res) => {
 		});
 });
 
+// Add exercises to the most recent workout plan
+router.put('/api/workouts/:id', (req, res) => {
+	Workout.updateOne(
+		{ _id: req.params.id },
+		{ $push: { exercises: req.body } },
+		(err, dbWorkout) => {
+			if (err) {
+				console.log(err);
+				res.sendStatus(500);
+			} else {
+				res.send(dbWorkout);
+			}
+		}
+	);
+});
+
 // View the total duration of each workout from the past seven workouts on the stats page
 router.get('/api/workouts/range', (req, res) => {
 	Workout.aggregate([
@@ -20,6 +36,22 @@ router.get('/api/workouts/range', (req, res) => {
 	])
 		.sort({ day: -1 })
 		.limit(7)
+		.then((dbWorkout) => {
+			res.json(dbWorkout);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.sendStatus(500);
+		});
+});
+
+// View the combined weight of multiple exercises from the past seven workouts on the stats page
+router.get('/api/workouts', (req, res) => {
+	Workout.aggregate([
+		{ $addFields: { totalDuration: { $sum: '$exercises.duration' } } },
+	])
+		.sort({ day: -1 })
+		.limit(1)
 		.then((dbWorkout) => {
 			res.json(dbWorkout);
 		})
